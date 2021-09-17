85e5715cea5c85d320cc5a34a9c9376c263bbb5c
pstaal <peter.staal@gmail.com>
2021-09-17 10:07:42 +0200

    add list of products to productlist component

--- my_app/src/app/product-list/product-list.component.ts
+++ my_app/src/app/product-list/product-list.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from '../../../data.json';
 
+type Product = {
+  id: number,
+  name: string,
+  price: number,
+  url: string,
+  description: string
+};
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  
+  products: Product[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.products = data;
   }
 
 }
