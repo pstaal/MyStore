59be795883b81aafb482929b11a066b15cb3aa90
pstaal <peter.staal@gmail.com>
2021-09-23 13:47:40 +0200

    add cart service functions

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../services/products.service';
+import { CartService } from '../services/cart.service';
+import { CartItem } from '../services/cart.service';
 
 export type Product = {
   id?: number,
@@ -18,10 +20,17 @@ export class ProductListComponent implements OnInit {
   
   products: Product[] = [];
 
-  constructor(private productService: ProductsService) { }
+  constructor(private productService: ProductsService, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.products = this.productService.getProducts();
   }
 
+  addItem() {
+   const item: CartItem = {
+     name:
+     price:
+     quantity:
+   }
+  }
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+export type CartItem = {
+  name?: string,
+  price?: number,
+  quantity?: number
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cart: CartItem[] = [];
+
+  constructor() { }
+
+  addItem(item: CartItem) {
+    if (this.cart.some(cartitem => cartitem.name === item.name)) {
+      let index = this.cart.findIndex(cartitem => cartitem.name === item.name);
+      let object = this.cart.find(cartitem => cartitem.name === item.name)!;
+      this.cart.splice(index,1);
+      object.quantity = object.quantity! + item.quantity!;
+      this.cart.push(object);
+      alert('Added product to cart!')
+    } else {
+    this.cart.push(item);
+    alert('Added product to cart!')
+    };
+  }
+
+  getItems() {
+    return this.cart;
+  }
+
+}
