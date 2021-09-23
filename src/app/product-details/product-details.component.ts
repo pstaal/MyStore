5840c236875814014bc20700ea19c5fd08d0b09c
pstaal <peter.staal@gmail.com>
2021-09-23 18:33:44 +0200

    add same select logic and add cart item to product details component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Product } from '../product-list/product-list.component';
 import { ActivatedRoute } from '@angular/router';
+import { quantities } from '../quantities';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,13 +13,24 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
 
   product: Product = {};
+  quantities: number[] = quantities;
+  quantity: number = quantities[0];
 
-  constructor(private productService: ProductsService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductsService, private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit(): void {
     let products = this.productService.getProducts();
     let id = parseInt(this.route.snapshot.params['id']);
     this.product = products.find(product => product.id === id)!;
+  }
+
+  addItem(name: string, price: number, quantity: number) {
+    let cartitem = { name, price, quantity };
+    this.cartService.addItem(cartitem);
+  }
+
+  onChange(quantity: number) {
+    this.quantity = quantity;
   }
 
 }
